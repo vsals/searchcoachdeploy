8510223358bef408797eb95716950bb61d54bcda
vsals <59352209+vsals@users.noreply.github.com>
2021-02-10 14:31:56 +0530

    Add files via upload

---
+++ Source/Microsoft.Teams.Apps.SearchCoach/Models/Configuration/AzureActiveDirectorySettings.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="AzureActiveDirectorySettings.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.SearchCoach.Models.Configuration
+{
+    /// <summary>
+    /// A class which helps to provide Azure Active Directory settings for application.
+    /// </summary>
+    public class AzureActiveDirectorySettings
+    {
+        /// <summary>
+        /// Gets or sets Graph API scope.
+        /// </summary>
+        public string GraphScope { get; set; }
+
+        /// <summary>
+        /// Gets or sets application tenant id.
+        /// </summary>
+        public string TenantId { get; set; }
+    }
+}
---
+++ Source/Microsoft.Teams.Apps.SearchCoach/Models/Configuration/BotSettings.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="BotSettings.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.SearchCoach.Models.Configuration
+{
+    /// <summary>
+    /// A class which helps to provide Bot settings for application.
+    /// </summary>
+    public class BotSettings
+    {
+        /// <summary>
+        /// Gets or sets application base Uri which helps in generating customer token.
+        /// </summary>
+        public string AppBasePath { get; set; }
+
+        /// <summary>
+        /// Gets or sets application id.
+        /// </summary>
+        public string MicrosoftAppId { get; set; }
+
+        /// <summary>
+        /// Gets or sets application password.
+        /// </summary>
+        public string MicrosoftAppPassword { get; set; }
+
+        /// <summary>
+        /// Gets or sets application manifest id.
+        /// </summary>
+        public string ManifestId { get; set; }
+
+        /// <summary>
+        /// Gets or sets card cache interval.
+        /// </summary>
+        public double CardCacheDurationInHour { get; set; }
+
+        /// <summary>
+        /// Gets or sets cache interval.
+        /// </summary>
+        public double CacheDurationInMinutes { get; set; }
+    }
+}
